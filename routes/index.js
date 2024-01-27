7940cabec72c11ee01e7043063906e15b4de0d56
Webdeveloperintraining <sil22006@byui.edu>
2024-01-27 12:18:59 -0800

    new message

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 router.use('/',require('./swagger'))
 
 router.get('/',(req,res) => {
-    res.send('Second project in development')});
+    res.send('Hello World!! This is the CSE341Project2')});
 
 router.use('/books', require('./books'));
 
